c7d9876383f1006f7ccf80e836734de676449c93
Jeevesh Lodhi <lodhijeevesh2000@gmail.com>
2020-11-21 16:44:02 +0530

    Updates

---
+++ precache-manifest.0cb594e98dabfc298677e9bcaeb1159d.js
@@ -0,0 +1,258 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e12607c6bf84c2ed98cc8792fce8283",
+    "url": "./index.html"
+  },
+  {
+    "revision": "acc00f72b7234444d9b1",
+    "url": "./static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "bf35a92e1118bebae1ae",
+    "url": "./static/css/3.3a26ebde.chunk.css"
+  },
+  {
+    "revision": "b91a21e08c1dea406336",
+    "url": "./static/css/4.ee2a0142.chunk.css"
+  },
+  {
+    "revision": "226210032a221fc441ec",
+    "url": "./static/css/main.067e58d2.chunk.css"
+  },
+  {
+    "revision": "acc00f72b7234444d9b1",
+    "url": "./static/js/2.317f9306.chunk.js"
+  },
+  {
+    "revision": "bf35a92e1118bebae1ae",
+    "url": "./static/js/3.ea1a2a58.chunk.js"
+  },
+  {
+    "revision": "b91a21e08c1dea406336",
+    "url": "./static/js/4.3d8816e3.chunk.js"
+  },
+  {
+    "revision": "226210032a221fc441ec",
+    "url": "./static/js/main.79c2b9d6.chunk.js"
+  },
+  {
+    "revision": "3896e31b15022b46eb21",
+    "url": "./static/js/runtime-main.6bfa876e.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "./static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "./static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "e3df670263148e715bc5f4a895212a62",
+    "url": "./static/media/adelfox1.e3df6702.png"
+  },
+  {
+    "revision": "1058d88cf92243f0fbf9c6f64ef5af6b",
+    "url": "./static/media/adelfox2.1058d88c.png"
+  },
+  {
+    "revision": "8e63b48a689bba980f5ee2626d9d8817",
+    "url": "./static/media/adelfox3.8e63b48a.png"
+  },
+  {
+    "revision": "3f2427b30a6077be092ce740fbb6aded",
+    "url": "./static/media/adelfox4.3f2427b3.png"
+  },
+  {
+    "revision": "4bf5b30ea69094cfa26ebc36c35dd962",
+    "url": "./static/media/adelfox5.4bf5b30e.png"
+  },
+  {
+    "revision": "ad14644525f0ba7b61309fbc9148143b",
+    "url": "./static/media/adelfox6.ad146445.png"
+  },
+  {
+    "revision": "ea200730a0de13be5f05bd5e80ad2a77",
+    "url": "./static/media/adelfox7.ea200730.png"
+  },
+  {
+    "revision": "985b99c8226e8edf3b4351fbc04caf4e",
+    "url": "./static/media/aftereffects.985b99c8.png"
+  },
+  {
+    "revision": "c59720d63dc3595a12e1c7e641a1bfa5",
+    "url": "./static/media/airbnbLogo.c59720d6.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "./static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "./static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "./static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "./static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "8ddf76a14a2e3ad3ba62b46d49a75a74",
+    "url": "./static/media/facebookLogo.8ddf76a1.png"
+  },
+  {
+    "revision": "c24ba79111a90758a3a472a906ab2901",
+    "url": "./static/media/googleAssistant.c24ba791.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "./static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "c00a812c269b2b92d490cb19d2e731ef",
+    "url": "./static/media/illustrator.c00a812c.png"
+  },
+  {
+    "revision": "591ee40c66f8ebe50b92e080c35c93a1",
+    "url": "./static/media/inkscape.591ee40c.png"
+  },
+  {
+    "revision": "b05ae5b575a7e1125610944dc152c35f",
+    "url": "./static/media/jsFramework.b05ae5b5.svg"
+  },
+  {
+    "revision": "1821dcdfe4b4bbc78e6f08338eb3a7ab",
+    "url": "./static/media/lightroom.1821dcdf.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "./static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "8d41c385078e5bfdb09da1ddc1e3ebe9",
+    "url": "./static/media/music.8d41c385.jpg"
+  },
+  {
+    "revision": "ff1d039d751fa7019deedfa1d735c673",
+    "url": "./static/media/music1.ff1d039d.png"
+  },
+  {
+    "revision": "d3f222e2b4a010680614e9b8d6f33baa",
+    "url": "./static/media/music2.d3f222e2.png"
+  },
+  {
+    "revision": "74b6fcd94f88c1340a9044e3630a4e5c",
+    "url": "./static/media/music3.74b6fcd9.png"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "./static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "f607bbdda54ed659a0ab6d24d7910d73",
+    "url": "./static/media/photoshop.f607bbdd.png"
+  },
+  {
+    "revision": "c3780a10bfef86b98b00022224a38bef",
+    "url": "./static/media/premiere.c3780a10.png"
+  },
+  {
+    "revision": "c1a95f7335cbfb730b937e19e8830a53",
+    "url": "./static/media/programmer.c1a95f73.svg"
+  },
+  {
+    "revision": "4711454bec62984831ed205bc4cbc07c",
+    "url": "./static/media/psit.4711454b.jpg"
+  },
+  {
+    "revision": "1a60e8a9e2c2d6a539c7692f27fe6790",
+    "url": "./static/media/pwa.1a60e8a9.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "./static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "4b2938b9f5ea2f1ddbd45dccaff628bc",
+    "url": "./static/media/quoraLogo.4b2938b9.png"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "./static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "./static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "113a9d4a7ddf351fc459150bba8ecd05",
+    "url": "./static/media/spitz.4.113a9d4a.jpg"
+  },
+  {
+    "revision": "70ef5c50a56b8419144910a00be3b179",
+    "url": "./static/media/spitz.5.70ef5c50.jpg"
+  },
+  {
+    "revision": "2e4ab41c300650406f256df8ea9e5214",
+    "url": "./static/media/spitz.6.2e4ab41c.jpg"
+  },
+  {
+    "revision": "562492b012e031a8c60f282bcde5de82",
+    "url": "./static/media/spitz.7.562492b0.jpg"
+  },
+  {
+    "revision": "96bce2df6187df6e640858d05c67ebdf",
+    "url": "./static/media/spitz.96bce2df.jpg"
+  },
+  {
+    "revision": "03f17943c21a8f6a0b206bed03acaf1f",
+    "url": "./static/media/spitz1.03f17943.jpg"
+  },
+  {
+    "revision": "4018b61b6d9a467dcef34c7f409170c4",
+    "url": "./static/media/spitz2.4018b61b.jpg"
+  },
+  {
+    "revision": "24f9b56e5266562b7b1781c46d760d6f",
+    "url": "./static/media/spitz3.24f9b56e.jpg"
+  },
+  {
+    "revision": "39eff4a91b01e00a32cb78f3797eec88",
+    "url": "./static/media/talksCardBack.39eff4a9.svg"
+  },
+  {
+    "revision": "7f0feeedda08cf7d68593454bc2e82b5",
+    "url": "./static/media/talksCardBackAlt.7f0feeed.svg"
+  },
+  {
+    "revision": "9fcd2d3569edfde63fffc2fbdc9fc695",
+    "url": "./static/media/weredoo1.9fcd2d35.PNG"
+  },
+  {
+    "revision": "a7bd9aa7bf185c1f1779c449a5b3c9ce",
+    "url": "./static/media/weredoo2.a7bd9aa7.PNG"
+  },
+  {
+    "revision": "a180032be1357cf2241d4ca6ad4cf461",
+    "url": "./static/media/weredoo3.a180032b.PNG"
+  },
+  {
+    "revision": "4900637cdefc1b7418e6596d4f498c1b",
+    "url": "./static/media/weredoo4.4900637c.PNG"
+  },
+  {
+    "revision": "9ce2c4a8a8162cb01b7b919ddea23d33",
+    "url": "./static/media/weredoo5.9ce2c4a8.PNG"
+  },
+  {
+    "revision": "5e9ccd1a747bf4d60e3197d5a9e2a78a",
+    "url": "./static/media/weredoo7.5e9ccd1a.png"
+  },
+  {
+    "revision": "3338afff05f1510f94c47fec823f3eb9",
+    "url": "./static/media/xd.3338afff.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.847aa2c4a27158bdddb53bc2e160354c.js"
+  "./precache-manifest.0cb594e98dabfc298677e9bcaeb1159d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
